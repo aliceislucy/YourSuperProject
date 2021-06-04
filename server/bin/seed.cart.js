5d6464d3a28920fbcf2c729c08590347a6bbce90
Alice LEFEVRE <alefevre.contact@gmail.com>
2021-06-04 12:57:15 +0200

    complicated random seeding for seed.cart.js + seed.comment.js

--- server/bin/seed.cart.js
+++ server/bin/seed.cart.js
@@ -3,7 +3,17 @@ const CartModel = require("../models/Cart");
 const UserModel = require("../models/User");
 const ProductModel = require("../models/Product");
 
-const products = [
+const carts = [
+  {
+    user: "",
+    products: [],
+    price: 0,
+  },
+  {
+    user: "",
+    products: [],
+    price: 0,
+  },
   {
     user: "",
     products: [],
@@ -16,11 +26,38 @@ const products = [
   },
 ];
 
-CartModel.deleteMany()
-  .then(() => CartModel.create(carts))
-  .then((documents) => {
-    console.log("THE CART SEEDS HAVE BEEN PLANTED 🌱");
-    console.log(documents);
+UserModel.find()
+  .then((userDocuments) => {
+    for (let i = 0; i < carts.length; i++) {
+      carts[i].user = userDocuments[i]._id;
+    }
+    ProductModel.find()
+      .then((productDocuments) => {
+        carts.forEach((cart) => {
+          const randomIndex = Math.floor(
+            Math.random() * (productDocuments.length - 1 - 0 + 1) + 0
+          );
+          cart.products.push(
+            productDocuments[randomIndex]._id,
+            productDocuments[randomIndex]._id,
+            productDocuments[randomIndex]._id,
+            productDocuments[randomIndex]._id,
+            productDocuments[randomIndex]._id
+          );
+        });
+        CartModel.deleteMany()
+          .then(() => CartModel.create(carts))
+          .then((documents) => {
+            console.log("THE CART SEEDS HAVE BEEN PLANTED 🌱");
+            console.log(documents);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   })
   .catch((error) => {
     console.log(error);
