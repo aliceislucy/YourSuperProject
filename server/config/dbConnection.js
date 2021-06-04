6a8d97cac63e220be6f4c02b5ef6a41545591105
Pol Seznec <johndoe@example.com>
2021-06-04 09:21:17 +0200

    Msg back-end server

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -8,7 +8,7 @@ mongoose
     useFindAndModify: false,
   })
   .then((self) => {
-    console.log(`Connected to ${self.connection.name} backend server <3`);
+    console.log(`Connected to ${self.connection.name} backend server <3 => http://localhost:5000/`);
   })
   .catch((err) => {
     console.log(`An error occurred while connecting to the database...`);
