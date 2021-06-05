9bae30f471c501561388e6cf4d711161c4554116
Pol Seznec <johndoe@example.com>
2021-06-05 16:43:02 +0200

    Subscribe page and From Comp Created and linked (j'ai comment le code de la form car il fais craché le site)

---
+++ client/src/pages/Subscribe.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SubscribeForm from "./../components/SubscribeForm"
+
+class Subscribe extends React.Component {
+  render() {
+    return (
+      <div>
+        <SubscribeForm />
+      </div>
+    );
+  }
+}
+
+export default Subscribe;
