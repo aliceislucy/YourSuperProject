9be369a27fc4c94d38512983da5d0c207a61e2c4
Pol Seznec <johndoe@example.com>
2021-06-06 17:43:56 +0200

    Try connect the subcribeForm companent to the subcribe page. add bug :/

--- client/src/components/SubscribeForm.jsx
+++ client/src/components/SubscribeForm.jsx
@@ -1,22 +1,45 @@
-// import React from "react";
-// import { withRouter, Redirect } from "react-router-dom";
+import React, { Component } from "react";
+//import { withRouter } from "react-router-dom";
+import apiHandler from "./../apiHandler";
 
-// class SubscribeForm extends Component {
-//   state = {
-//     email: "",
-//   };
+class SubscribeForm extends Component {
+  state = {
+    email: "",
+    isMember: "",
+  };
 
-//   render() {
-//     return (
-//       <>
-//         <span>SubscribeForm</span>
-//         <form action=""></form>
-//         <label htmlFor="email">Email</label>
-//         <input type="email" id="email" name="email" />
-//         <button>Submit</button>
-//       </>
-//     );
-//   }
-// }
+  handleChange = (event) => {
+    const value = event.target.value;
+    const key = event.target.name;
+    this.setState({ [key]: value });
+  };
 
-// export default withRouter(withUser(SubscribeForm));
+  
+
+  render() {
+    return (
+      <section className="form-section">
+        <h2>SubscribeForm</h2>
+
+        <form
+          autoComplete="off"
+          className="form"
+          onSubmit={this.handleSubmit}
+          action=""
+        >
+          <label className="label" htmlFor="email" >Email</label>
+          <input
+            onChange={this.handleChange}
+            value={this.state.email}
+            type="email"
+            id="email"
+            name="email"
+          />
+          <button>Register</button>
+        </form>
+      </section>
+    );
+  }
+}
+
+export default SubscribeForm;
--- client/src/pages/Subscribe.jsx
+++ client/src/pages/Subscribe.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import SubscribeForm from "./../components/SubscribeForm"
+import SubscribeForm from "../components/SubscribeForm";
+
+// const Subscribe = () => {
+//   return <SubscribeForm />;
+// };
 
 class Subscribe extends React.Component {
   render() {
     return (
       <div>
+        
         <SubscribeForm />
       </div>
     );
