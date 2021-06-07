e43849ad7afb7327cba142e131456a950702296e
Pol Seznec <johndoe@example.com>
2021-06-07 10:51:22 +0200

    Front link Productdisplayeur

---
+++ client/src/components/ProductDisplayer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const ProductDisplayer = () => {
+  return (
+    <div>
+     <h2>ProductDisplayer</h2>
+    </div>
+  );
+};
+
+export default ProductDisplayer;
