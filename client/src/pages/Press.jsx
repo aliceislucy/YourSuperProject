4e48c436f636b1d109add180ada903d774417ae0
Pol Seznec <johndoe@example.com>
2021-06-07 16:40:49 +0200

    Press views connect to the app.jsx

---
+++ client/src/pages/Press.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Press extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Press's View</h1>
+            </div>
+        )
+    }
+}
+
+export default Press;
