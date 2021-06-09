9a6c1e7e55ae5a23e9cae121833095199a10e81f
Pol Seznec <johndoe@example.com>
2021-06-09 20:40:43 +0200

    add Logic in admin CRUD

--- client/src/admin/AdminHome.jsx
+++ client/src/admin/AdminHome.jsx
@@ -1,13 +1,56 @@
 import React from "react";
-import AdminProductList from "./AdminProductList"
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-const AdminHome = () => {
-  return (
-    <div>
-      <h1>YourSuper - Admin Home</h1>
-      <AdminProductList />
-    </div>
-  );
-};
-
+class AdminHome extends React.Component {  
+  state = {
+    products: [],
+  };
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/product/")
+      .then((response) => {
+        this.setState({ products: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  handleDelete = (productId) => {
+    axios
+      .delete("http://localhost:5000/api/product/" + productId)
+      .then((response) => {
+        this.setState({
+          products: this.state.products.filter((product) => {
+            return product._id !== productId;
+          }),
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  render() {
+    return (
+      <div className="AdminProductList">
+        <h1>YourSuper - Admin Home</h1>
+        {this.state.products.map((product) => {
+          return (
+            <div key={product._id} className="Product">
+              {/* <Link to={`/products/${product._id}`}>See details </Link> */}
+              <div>
+                <img src={product.productImg} alt={product.name} />
+              </div>
+              <h4>{product.name}</h4>
+              <button onClick={() => this.handleDelete(product._id)}>
+                Delete
+              </button>
+              <Link to={`/products/edit/${product._id}`}>Edit</Link>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
 export default AdminHome;
--- client/src/admin/AdminProductList.jsx
+++ client/src/admin/AdminProductList.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-class AdminProductList extends Component {
+class AdminProductList extends Component {  
   state = {
     products: [],
   };
