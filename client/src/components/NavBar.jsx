c399a7c461a5e328ff9d4d67c0287dca0a443b03
Pol Seznec <johndoe@example.com>
2021-06-10 09:58:39 +0200

    Font-style Added + Some css on navBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -29,7 +29,7 @@ const NavBar = (props) => {
               className="navlink"
               exact
               to="/products"
-              style={{ textDecoration: "none" }}
+              style={{ textDecoration: "none", color: "black" }}
             >
               Products
             </NavLink>
